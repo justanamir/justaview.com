ec4b9074f03f221eeb757cfa16cfa7df7088c485
justanamir <amirdanialmuslim@gmail.com>
2021-01-11 23:04:43 +0800

    created a controller folder that would handle all user pages

--- main.go
+++ main.go
@@ -3,28 +3,29 @@ package main
 import (
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"justaview.com/controllers"
 	"justaview.com/views"
+
+	"github.com/gorilla/mux"
 )
 
 var (
 	homeView    *views.View
 	contactView *views.View
 	faqView     *views.View
-	signupView  *views.View
 )
 
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	faqView = views.NewView("bootstrap", "views/faq.gohtml")
-	signupView = views.NewView("bootstrap", "views/signup.gohtml")
+	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/signup", usersC.New)
 	http.ListenAndServe(":3000", r)
 }
 
@@ -41,11 +42,6 @@ func contact(w http.ResponseWriter, r *http.Request) {
 func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(faqView.Render(w, nil))
-}
-
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
 }
 
 //A helper function that panics on any error
